fe69897e58997bf3fa93cbbe39a0c9df37fb739b
Subhendu Hazra <subhenduhazra@Subhendus-MacBook-Air.local>
2022-10-24 19:55:46 +0530

    downloaded old

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php ?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Riverslide Games</title>
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+        <h1 id="bigHeading">Riverslide Games</h1>
+        <div id="games_container">
+            <a href="tictactoe" class="game_link">
+                <div class="game_tile">
+                    <img src="tictactoe/images/about_logo.png" />
+                    <figcaption>Tic Tac Toe</figcaption>
+                </div>
+            </a>
+            <a href="minesweeper" class="game_link">
+                <div class="game_tile">
+                    <img src="minesweeper/images/about_logo.png" />
+                    <figcaption>Minesweper</figcaption>
+                </div>
+            </a>
+            <a href="snake" class="game_link">
+                <div class="game_tile">
+                    <img src="snake/images/about_logo.png" />
+                    <figcaption>Snake</figcaption>
+                </div>
+            </a>
+        </div>
+        <footer> <div id="company">Riverslide</div> <div id="year">2022</div> </footer>
+        
+    </body>
+</html>
