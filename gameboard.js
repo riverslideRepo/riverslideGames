a487a6402b7307b32b928f6f91c7ec969eda9ae8
Subhendu Hazra <subhendu965.hazra@gmail.com>
2022-10-25 01:31:11 +0530

    style for menu in gameboard adjusted

--- gameboard.js
+++ gameboard.js
@@ -184,13 +184,7 @@ header.open_menu #menu{
 }
 /*---------*/
 header #menu #middle{
-    height:100%;
-    display:flex;
-    flex-direction: column;
-    gap:50px;
-
-    align-items:center;
-    justify-content: center;
+   
 }
 
 header #menu #bottom{
